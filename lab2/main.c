6528f70aa8ded9aa075149339852147d3247ded6
Степан Ерохин <se6350235@gmail.com>
2024-09-09 15:48:37 +0300

    1

--- lab2/main.c
+++ lab2/main.c
@@ -5,17 +5,15 @@ int main()
 {
 	setlocale(LC_CTYPE, "RUS");
 
-	int n = 3, L = 333;
-	int	k = 3, m = 3;
+	int n = 3;
 
 	printf("Дано:\n");
 	printf("\t%d\n", n);
-	printf("\t%d\n", L);
+	printf("\t%d\n", 333);
 	printf("       ------\n");
 
 	printf("Oтвет:\n");
-	printf("\t%.*d", k, (n / L));
-	printf(".%.*d", m, (n % L));
+	printf("%07.3f", (n / 333.));
 
 	return 0;
 }
